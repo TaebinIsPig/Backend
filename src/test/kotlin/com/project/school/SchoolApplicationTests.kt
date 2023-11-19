08c784ddcf32ecec1b4ff60959efc36c61e2d59e
yoontaebin <gusckd000@gmail.com>
2023-11-20 01:42:17 +0900

    Fix :: CI error

--- src/test/kotlin/com/project/school/SchoolApplicationTests.kt
+++ src/test/kotlin/com/project/school/SchoolApplicationTests.kt
@@ -3,7 +3,6 @@ package com.project.school
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class SchoolApplicationTests {
 
     @Test
