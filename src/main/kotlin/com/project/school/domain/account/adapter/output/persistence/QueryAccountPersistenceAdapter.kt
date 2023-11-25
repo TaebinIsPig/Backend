edf34d0ad0465b709f87d8851b6f9a197ae5bd5d
yoontaebin <gusckd000@gmail.com>
2023-11-25 18:50:34 +0900

    Add :: SecurityConfig

--- src/main/kotlin/com/project/school/domain/account/adapter/output/persistence/QueryAccountPersistenceAdapter.kt
+++ src/main/kotlin/com/project/school/domain/account/adapter/output/persistence/QueryAccountPersistenceAdapter.kt
@@ -1,6 +1,5 @@
 package com.project.school.domain.account.adapter.output.persistence
 
-import com.project.school.domain.account.adapter.output.persistence.mapper.AccountMapper
 import com.project.school.domain.account.adapter.output.persistence.repository.AccountRepository
 import com.project.school.domain.account.application.port.output.QueryAccountPort
 import org.springframework.stereotype.Component
