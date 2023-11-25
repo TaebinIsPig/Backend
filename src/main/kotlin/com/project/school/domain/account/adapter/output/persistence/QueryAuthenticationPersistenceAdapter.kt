6f392a12e02163e42061fd9adb6346a348f9e72f
yoontaebin <gusckd000@gmail.com>
2023-11-25 18:41:37 +0900

    Add :: QueryAuthenticationPersistenceAdapter

---
+++ src/main/kotlin/com/project/school/domain/account/adapter/output/persistence/QueryAuthenticationPersistenceAdapter.kt
@@ -0,0 +1,21 @@
+package com.project.school.domain.account.adapter.output.persistence
+
+import com.project.school.domain.account.adapter.output.persistence.mapper.AuthenticationMapper
+import com.project.school.domain.account.adapter.output.persistence.repository.AuthenticationRepository
+import com.project.school.domain.account.application.port.output.QueryAuthenticationPort
+import com.project.school.domain.account.domain.Authentication
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class QueryAuthenticationPersistenceAdapter(
+    private val authenticationRepository: AuthenticationRepository,
+    private val authenticationMapper: AuthenticationMapper
+): QueryAuthenticationPort {
+
+    override fun findByPhoneNumberOrNull(phoneNumber: String): Authentication? {
+        val authenticationEntity = authenticationRepository.findByIdOrNull(phoneNumber)
+        return authenticationMapper.toDomain(authenticationEntity)
+    }
+
+}
