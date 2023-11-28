d1971e37db94119dfaa48126a503892bf40d50fe
yoontaebin <gusckd000@gmail.com>
2023-11-28 20:59:48 +0900

    Add :: increaseAuthenticationCount 메서드

--- src/main/kotlin/com/project/school/domain/account/domain/Authentication.kt
+++ src/main/kotlin/com/project/school/domain/account/domain/Authentication.kt
@@ -1,9 +1,23 @@
 package com.project.school.domain.account.domain
 
+import com.project.school.common.annotation.RootAggregate
+
+@RootAggregate
 data class Authentication(
     val phoneNumber: String,
     val authCodeCount: Long,
     val authenticationCount: Long,
     val isVerified: Boolean,
     val expiredAt: Long
-)
+) {
+
+    companion object {
+        const val EXPIRED_AT = 7200L
+    }
+
+    fun increaseAuthenticationCount(): Authentication =
+        this.copy(
+            authenticationCount = authenticationCount.inc()
+        )
+
+}
