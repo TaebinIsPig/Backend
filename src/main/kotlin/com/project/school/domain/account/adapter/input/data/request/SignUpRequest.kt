06dc6953d252a400d8692ef366009b26d7ded3cb
yoontaebin <gusckd000@gmail.com>
2023-11-29 01:16:32 +0900

    Update :: 정규식 변경 및 message 추가

--- src/main/kotlin/com/project/school/domain/account/adapter/input/data/request/SignUpRequest.kt
+++ src/main/kotlin/com/project/school/domain/account/adapter/input/data/request/SignUpRequest.kt
@@ -1,30 +1,31 @@
 package com.project.school.domain.account.adapter.input.data.request
 
 import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class SignUpRequest(
-    @field:NotNull
-    @field:Length(min = 6, max = 15)
+    @field:NotBlank(message = "아이디는 필수값입니다.")
+    @field:Length(min = 6, max = 15, message = "id의 글자 수는 8 ~ 15자 사이여야 합니다.")
     val id: String,
 
-    @field:NotNull
-    @field:Length(min = 8, max = 20)
-    @field:Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*?~])[0-9a-zA-Z!@#$%^&*?~]+$")
+    @field:NotBlank(message = "비밀번호는 필수값입니다.")
+    @field:Length(min = 8, max = 20, message = "비밀번호의 글자 수는 8 ~ 20자 사이여야 합니다.")
+    @field:Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%^&*?~])[a-zA-Z!@#\$%^&*?~]+\$", message = "비밀번호는 대소문자와 특수문자를 한개씩 포함하여야 합니다.")
     val password: String,
 
-    @field:NotNull
-    @field:Size(min = 2, max = 10)
+    @field:NotBlank(message = "이름은 필수값입니다.")
+    @field:Size(min = 2, max = 10, message = "이름의 글자 수는 2 ~ 10자 사이여야 합니다.")
     val name: String,
 
     val studentNumber: StudentNumberRequest,
 
-    @field:NotNull
-    @field:Size(min = 10, max = 11)
+    @field:NotBlank(message = "전화번호는 필수값입니다.")
+    @field:Size(min = 10, max = 11, message = "전화번호의 글자 수는 10 ~ 11자 사이여야 합니다.")
     val phoneNumber: String,
 
-    @field:NotNull
+    @field:NotBlank(message = "학교는 필수값입니다.")
     val school: String
 )
--- src/main/kotlin/com/project/school/domain/account/adapter/input/data/request/StudentNumberRequest.kt
+++ src/main/kotlin/com/project/school/domain/account/adapter/input/data/request/StudentNumberRequest.kt
@@ -3,12 +3,12 @@ package com.project.school.domain.account.adapter.input.data.request
 import javax.validation.constraints.NotNull
 
 data class StudentNumberRequest(
-    @field:NotNull
+    @field:NotNull(message = "학년은 필수값입니다.")
     val grade: Int,
 
-    @field:NotNull
+    @field:NotNull(message = "반은 필수값입니다.")
     val classNum: Int,
 
-    @field:NotNull
+    @field:NotNull(message = "번호는 필수값입니다.")
     val number: Int
 )
