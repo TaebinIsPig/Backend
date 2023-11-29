e29dcba4a9c1b1dd4655034931faea1628f82e63
yoontaebin <gusckd000@gmail.com>
2023-11-30 03:12:19 +0900

    Add :: JwtProperties

--- src/main/kotlin/com/project/school/global/config/PropertiesScanConfig.kt
+++ src/main/kotlin/com/project/school/global/config/PropertiesScanConfig.kt
@@ -2,6 +2,7 @@ package com.project.school.global.config
 
 import com.project.school.domain.account.adapter.output.message.properties.CoolSmsProperties
 import com.project.school.global.security.token.properties.JwtExpTimeProperties
+import com.project.school.global.security.token.properties.JwtProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationPropertiesScan(
     basePackageClasses = [
         CoolSmsProperties::class,
-        JwtExpTimeProperties::class
+        JwtExpTimeProperties::class,
+        JwtProperties::class
     ]
 )
 class PropertiesScanConfig
