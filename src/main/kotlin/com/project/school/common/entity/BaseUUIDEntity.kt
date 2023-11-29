62848d6c5fad1420230ecb783f229475d46a926f
yoontaebin <gusckd000@gmail.com>
2023-11-30 03:35:38 +0900

    Update :: accountIdx로 변경

--- src/main/kotlin/com/project/school/common/entity/BaseUUIDEntity.kt
+++ src/main/kotlin/com/project/school/common/entity/BaseUUIDEntity.kt
@@ -13,5 +13,5 @@ abstract class BaseUUIDEntity(
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)", nullable = false)
-    open val idx: UUID
+    open val accountIdx: UUID
 )
