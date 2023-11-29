440d47fc1ac33b53ab614eea105e3cea1e535178
yoontaebin <gusckd000@gmail.com>
2023-11-30 04:20:06 +0900

    Add :: PasswordNotMatchException

--- src/main/kotlin/com/project/school/global/error/ErrorCode.kt
+++ src/main/kotlin/com/project/school/global/error/ErrorCode.kt
@@ -8,6 +8,7 @@ enum class ErrorCode(
     // ACCOUNT
     DUPLICATE_ACCOUNT_ID("중복된 id 입니다.", 409),
     ACCOUNT_NOT_FOUND("계정을 찾을 수 없습니다.", 404),
+    PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다.", 400),
 
     // AUTHENTICATION
     AUTHENTICATION_NOT_FOUND("인증되지 않은 사용자 입니다.", 404),
