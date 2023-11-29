2b19a1e7286ab58ef947901303b538f4c1d6c293
yoontaebin <gusckd000@gmail.com>
2023-11-29 23:33:26 +0900

    Add :: QueryAuthCodePersistenceAdapter

---
+++ src/main/kotlin/com/project/school/domain/account/adapter/output/persistence/QueryAuthCodePersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.project.school.domain.account.adapter.output.persistence
+
+import com.project.school.domain.account.adapter.output.persistence.mapper.AuthCodeMapper
+import com.project.school.domain.account.adapter.output.persistence.repository.AuthCodeRepository
+import com.project.school.domain.account.application.port.output.QueryAuthCodePort
+import com.project.school.domain.account.domain.AuthCode
+import org.springframework.data.repository.findByIdOrNull
+
+class QueryAuthCodePersistenceAdapter(
+    private val authCodeRepository: AuthCodeRepository,
+    private val authCodeMapper: AuthCodeMapper
+): QueryAuthCodePort {
+
+    override fun findByPhoneNumberOrNull(phoneNumber: String): AuthCode? {
+        val authCodeEntity = authCodeRepository.findByIdOrNull(phoneNumber)
+        return authCodeMapper.toDomain(authCodeEntity)
+    }
+
+}
