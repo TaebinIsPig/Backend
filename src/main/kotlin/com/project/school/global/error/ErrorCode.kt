3a366c63c16f4b0e16abd8d838b4d8e666e74255
yoontaebin <gusckd000@gmail.com>
2023-11-29 23:38:13 +0900

    Add :: TooManyAuthCodeRequestException

--- src/main/kotlin/com/project/school/global/error/ErrorCode.kt
+++ src/main/kotlin/com/project/school/global/error/ErrorCode.kt
@@ -14,6 +14,7 @@ enum class ErrorCode(
 
     // AUTH CODE
     AUTH_CODE_NOT_FOUND("인증 코드를 찾을 수 없습니다.", 404),
+    TOO_MANY_AUTH_CODE_REQUEST("인증 코드 확인 요청을 5번 초과한 사용자 입니다.", 429),
 
     // MESSAGE
     MESSAGE_SEND_FAILED("coolsms 메세지 전송에 실패하였습니다.", 500)
