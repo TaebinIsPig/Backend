a825eb19c91c36698dc8d7215a6cfbe773098e82
yoontaebin <gusckd000@gmail.com>
2023-12-04 13:57:57 +0900

    Add :: updateEncodedPassword 메서드 추가

--- src/main/kotlin/com/project/school/domain/account/domain/Account.kt
+++ src/main/kotlin/com/project/school/domain/account/domain/Account.kt
@@ -7,10 +7,17 @@ import java.util.UUID
 data class Account(
     val accountIdx: UUID,
     val id: String,
-    val password: String,
+    var password: String,
     val name: String,
     val studentNumber: StudentNumber,
     val phoneNumber: String,
     val school: String,
     val authority: Authority
-)
+) {
+
+    fun updateEncodedPassword(encodedNewPassword: String): Account {
+        this.password = encodedNewPassword
+        return this
+    }
+
+}
