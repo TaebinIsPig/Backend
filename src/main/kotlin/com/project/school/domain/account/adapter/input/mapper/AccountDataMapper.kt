693fad9138c37ac7c993d2bf3a9ebb787ddecb37
yoontaebin <gusckd000@gmail.com>
2023-12-11 23:56:20 +0900

    Add :: toResponse 메서드 추가

--- src/main/kotlin/com/project/school/domain/account/adapter/input/mapper/AccountDataMapper.kt
+++ src/main/kotlin/com/project/school/domain/account/adapter/input/mapper/AccountDataMapper.kt
@@ -1,7 +1,10 @@
 package com.project.school.domain.account.adapter.input.mapper
 
 import com.project.school.domain.account.adapter.input.data.request.FindAccountPasswordRequest
+import com.project.school.domain.account.adapter.input.data.response.ProfileResponse
+import com.project.school.domain.account.adapter.input.data.response.StudentNumberResponse
 import com.project.school.domain.account.application.port.input.dto.FindAccountPasswordDto
+import com.project.school.domain.account.application.port.input.dto.ProfileDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,6 +15,18 @@ class AccountDataMapper {
             phoneNumber = request.phoneNumber,
             id = request.id,
             newPassword = request.newPassword
+        )
+
+    infix fun toResponse(profileDto: ProfileDto): ProfileResponse =
+        ProfileResponse(
+            name = profileDto.name,
+            phoneNumber = profileDto.phoneNumber,
+            studentNumber = StudentNumberResponse(
+                grade = profileDto.studentNumber.grade,
+                classNum = profileDto.studentNumber.classNum,
+                number = profileDto.studentNumber.number
+            ),
+            school = profileDto.school
         )
 
 }
