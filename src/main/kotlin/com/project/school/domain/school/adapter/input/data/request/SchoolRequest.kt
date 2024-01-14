88a9027bf3df4c3b6a8d6036f69260510fec44c5
yoontaebin <gusckd000@gmail.com>
2024-01-14 15:09:50 +0900

    Add :: SchoolRequest 추가

---
+++ src/main/kotlin/com/project/school/domain/school/adapter/input/data/request/SchoolRequest.kt
@@ -0,0 +1,8 @@
+package com.project.school.domain.school.adapter.input.data.request
+
+data class SchoolRequest(
+    val educationCode: String,
+    val adminCode: String,
+    val schoolName: String,
+    val schoolType: String
+)
