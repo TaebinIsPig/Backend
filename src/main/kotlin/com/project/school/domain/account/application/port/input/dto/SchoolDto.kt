d02212cbb204b808ffc3ebe2b980360ce06bdc58
yoontaebin <gusckd000@gmail.com>
2024-01-14 15:09:36 +0900

    Add :: SchoolDto 추가

---
+++ src/main/kotlin/com/project/school/domain/account/application/port/input/dto/SchoolDto.kt
@@ -0,0 +1,8 @@
+package com.project.school.domain.account.application.port.input.dto
+
+data class SchoolDto(
+    val educationCode: String,
+    val adminCode: String,
+    val schoolName: String,
+    val schoolType: String
+)
