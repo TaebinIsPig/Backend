4da67316f9ad21c1857baaadbd370a4bb90e44e3
yoontaebin <gusckd000@gmail.com>
2024-01-22 14:56:22 +0900

    Add :: updateProfile 추가

--- src/main/kotlin/com/project/school/domain/account/domain/Account.kt
+++ src/main/kotlin/com/project/school/domain/account/domain/Account.kt
@@ -1,6 +1,8 @@
 package com.project.school.domain.account.domain
 
 import com.project.school.common.annotation.RootAggregate
+import com.project.school.domain.account.application.port.input.dto.SchoolDto
+import com.project.school.domain.account.application.port.input.dto.StudentNumberDto
 import com.project.school.domain.school.domain.School
 import java.util.UUID
 
@@ -9,15 +11,33 @@ data class Account(
     val accountIdx: UUID,
     val id: String,
     var password: String,
-    val name: String,
-    val studentNumber: StudentNumber,
-    val phoneNumber: String,
-    val school: School,
+    var name: String,
+    var studentNumber: StudentNumber,
+    var phoneNumber: String,
+    var school: School,
     val authority: Authority
 ) {
 
     fun updateEncodedPassword(encodedNewPassword: String): Account {
         this.password = encodedNewPassword
+        return this
+    }
+
+    fun updateProfile(
+        name: String,
+        phoneNumber: String,
+        school: SchoolDto,
+        studentNumber: StudentNumberDto
+        ): Account {
+        this.name = name
+        this.phoneNumber = phoneNumber
+        this.school.educationCode = school.educationCode
+        this.school.adminCode = school.adminCode
+        this.school.schoolName = school.schoolName
+        this.school.schoolType = school.schoolType
+        this.studentNumber.grade = studentNumber.grade
+        this.studentNumber.classNum = studentNumber.classNum
+        this.studentNumber.number = studentNumber.number
         return this
     }
 
