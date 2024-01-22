ed0ffb94793faa244930766728d7f4cee8610672
yoontaebin <gusckd000@gmail.com>
2024-01-22 15:01:19 +0900

    Add :: 프로필 수정 서비스 추가

---
+++ src/main/kotlin/com/project/school/domain/account/application/service/UpdateAccountProfileService.kt
@@ -0,0 +1,41 @@
+package com.project.school.domain.account.application.service
+
+import com.project.school.common.annotation.ServiceWithTransaction
+import com.project.school.domain.account.application.common.util.AuthenticationValidator
+import com.project.school.domain.account.application.event.DeleteAuthenticationEvent
+import com.project.school.domain.account.application.exception.AccountNotFoundException
+import com.project.school.domain.account.application.port.input.UpdateAccountProfileUseCase
+import com.project.school.domain.account.application.port.input.dto.UpdateAccountProfileDto
+import com.project.school.domain.account.application.port.output.AccountSecurityPort
+import com.project.school.domain.account.application.port.output.CommandAccountPort
+import com.project.school.domain.account.application.port.output.QueryAccountPort
+import org.springframework.context.ApplicationEventPublisher
+
+@ServiceWithTransaction
+class UpdateAccountProfileService(
+    private val accountSecurityPort: AccountSecurityPort,
+    private val queryAccountPort: QueryAccountPort,
+    private val commandAccountPort: CommandAccountPort,
+    private val authenticationValidator: AuthenticationValidator,
+    private val publisher: ApplicationEventPublisher
+): UpdateAccountProfileUseCase {
+
+    override fun execute(dto: UpdateAccountProfileDto) {
+        val accountIdx = accountSecurityPort.getCurrentAccountIdx()
+        val account = queryAccountPort.findByIdxOrNull(accountIdx)
+            ?: throw AccountNotFoundException()
+
+        if (account.phoneNumber != dto.phoneNumber) {
+            val authentication = authenticationValidator.verifyAuthenticationByPhoneNumber(dto.phoneNumber)
+            publisher.publishEvent(DeleteAuthenticationEvent(authentication))
+        }
+
+        commandAccountPort.saveAccount(account.updateProfile(
+            name = dto.name,
+            phoneNumber = dto.phoneNumber,
+            school = dto.school,
+            studentNumber = dto.studentNumber
+        ))
+    }
+
+}
