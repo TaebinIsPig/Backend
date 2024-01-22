29867125c8291b468e98f6fa64f6fca6b0ac7607
yoontaebin <gusckd000@gmail.com>
2024-01-22 15:04:28 +0900

    Add :: toDto 추가

--- src/main/kotlin/com/project/school/domain/account/adapter/input/mapper/AccountDataMapper.kt
+++ src/main/kotlin/com/project/school/domain/account/adapter/input/mapper/AccountDataMapper.kt
@@ -1,14 +1,20 @@
 package com.project.school.domain.account.adapter.input.mapper
 
 import com.project.school.domain.account.adapter.input.data.request.FindAccountPasswordRequest
+import com.project.school.domain.account.adapter.input.data.request.UpdateAccountProfileRequest
 import com.project.school.domain.account.adapter.input.data.response.ProfileResponse
 import com.project.school.domain.account.adapter.input.data.response.StudentNumberResponse
 import com.project.school.domain.account.application.port.input.dto.FindAccountPasswordDto
 import com.project.school.domain.account.application.port.input.dto.ProfileDto
+import com.project.school.domain.account.application.port.input.dto.UpdateAccountProfileDto
+import com.project.school.domain.school.adapter.input.mapper.SchoolDataMapper
 import org.springframework.stereotype.Component
 
 @Component
-class AccountDataMapper {
+class AccountDataMapper(
+    private val studentNumberDataMapper: StudentNumberDataMapper,
+    private val schoolDataMapper: SchoolDataMapper
+) {
 
     infix fun toDto(request: FindAccountPasswordRequest): FindAccountPasswordDto =
         FindAccountPasswordDto(
@@ -27,6 +33,14 @@ class AccountDataMapper {
                 number = profileDto.studentNumber.number
             ),
             school = profileDto.school
+        )
+
+    infix fun toDto(request: UpdateAccountProfileRequest): UpdateAccountProfileDto =
+        UpdateAccountProfileDto(
+            name = request.name,
+            phoneNumber = request.phoneNumber,
+            school = schoolDataMapper.toDto(request.school),
+            studentNumber = studentNumberDataMapper.toDto(request.studentNumber)
         )
 
 }
