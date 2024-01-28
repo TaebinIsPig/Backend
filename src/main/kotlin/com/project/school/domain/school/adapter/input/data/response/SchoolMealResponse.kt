99dc1fcf3f6551a3d9199db230626f4fa4cef588
yoontaebin <gusckd000@gmail.com>
2024-01-28 23:34:10 +0900

    Add :: SchoolMealResponse 추가

---
+++ src/main/kotlin/com/project/school/domain/school/adapter/input/data/response/SchoolMealResponse.kt
@@ -0,0 +1,8 @@
+package com.project.school.domain.school.adapter.input.data.response
+
+data class SchoolMealResponse(
+    val mealType: String?,
+    val mealDate: String?,
+    val food: List<String>?,
+    val calorie: String?
+)
