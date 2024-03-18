f76106b6c0f47c3ccbab7ac01f70b43860d282b2
yoontaebin <gusckd000@gmail.com>
2024-03-18 23:32:56 +0900

    Update :: nullable로 변경

--- src/main/kotlin/com/project/school/domain/school/adapter/output/neis/response/NeisFindSchoolScheduleResponse.kt
+++ src/main/kotlin/com/project/school/domain/school/adapter/output/neis/response/NeisFindSchoolScheduleResponse.kt
@@ -1,7 +1,7 @@
 package com.project.school.domain.school.adapter.output.neis.response
 
 data class NeisFindSchoolScheduleResponse(
-    val eventDate: String,
-    val eventName: String,
+    val eventDate: String?,
+    val eventName: String?,
     val result: Result
 )
