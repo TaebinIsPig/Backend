392b42ba1688fa99d3f1df65c174ad0c27fecb39
yoontaebin <gusckd000@gmail.com>
2024-03-21 16:55:12 +0900

    Add :: toResponse 추가

--- src/main/kotlin/com/project/school/domain/school/adapter/input/mapper/SchoolDataMapper.kt
+++ src/main/kotlin/com/project/school/domain/school/adapter/input/mapper/SchoolDataMapper.kt
@@ -3,8 +3,10 @@ package com.project.school.domain.school.adapter.input.mapper
 import com.project.school.domain.account.application.port.input.dto.SchoolDto
 import com.project.school.domain.school.adapter.input.data.request.SchoolRequest
 import com.project.school.domain.school.adapter.input.data.response.SchoolMealResponse
+import com.project.school.domain.school.adapter.input.data.response.SchoolScheduleResponse
 import com.project.school.domain.school.adapter.input.data.response.SchoolSearchResponse
 import com.project.school.domain.school.application.port.input.dto.SchoolMealDto
+import com.project.school.domain.school.application.port.input.dto.SchoolScheduleDto
 import com.project.school.domain.school.application.port.input.dto.SchoolSearchDto
 import org.springframework.stereotype.Component
 
@@ -24,15 +26,18 @@ class SchoolDataMapper {
             isLast = schoolSearchDto.isLast,
             schoolList = schoolSearchDto.schoolList
         )
+    fun toResponse(dto: SchoolMealDto): SchoolMealResponse =
+        SchoolMealResponse(
+            mealType = dto.mealType,
+            mealDate = dto.mealDate,
+            food = dto.food,
+            calorie = dto.calorie
+        )
 
-    fun toResponse(dto: List<SchoolMealDto>): List<SchoolMealResponse> =
-        dto.map {
-            SchoolMealResponse(
-                mealType = it.mealType,
-                mealDate = it.mealDate,
-                food = it.food,
-                calorie = it.calorie
-            )
-        }
+    fun toResponse(dto: SchoolScheduleDto): SchoolScheduleResponse =
+        SchoolScheduleResponse(
+            eventDate = dto.eventDate,
+            eventName = dto.eventName
+        )
 
 }
