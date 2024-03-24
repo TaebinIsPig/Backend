4e11370dbafd204a035d41d32e43c71aa2c7655e
yoontaebin <gusckd000@gmail.com>
2024-03-24 14:08:46 +0900

    Update :: Aggregate 어노테이션 추가

--- src/main/kotlin/com/project/school/domain/school/domain/School.kt
+++ src/main/kotlin/com/project/school/domain/school/domain/School.kt
@@ -1,5 +1,8 @@
 package com.project.school.domain.school.domain
 
+import com.project.school.common.annotation.Aggregate
+
+@Aggregate
 data class School(
     var educationCode: String,
     var adminCode: String,
