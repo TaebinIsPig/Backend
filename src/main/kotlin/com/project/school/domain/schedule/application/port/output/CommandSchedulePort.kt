eca7192e8f45cbbad1bedf81cda2039bdd7c4ac7
yoontaebin <gusckd000@gmail.com>
2024-06-25 23:04:06 +0900

    Add :: deleteAllByAccount

--- src/main/kotlin/com/project/school/domain/schedule/application/port/output/CommandSchedulePort.kt
+++ src/main/kotlin/com/project/school/domain/schedule/application/port/output/CommandSchedulePort.kt
@@ -1,9 +1,12 @@
 package com.project.school.domain.schedule.application.port.output
 
 import com.project.school.domain.schedule.domain.Schedule
+import java.util.UUID
 
 interface CommandSchedulePort {
 
     fun saveSchedule(schedule: Schedule)
+
+    fun deleteAllByAccount(accountId: UUID)
 
 }
