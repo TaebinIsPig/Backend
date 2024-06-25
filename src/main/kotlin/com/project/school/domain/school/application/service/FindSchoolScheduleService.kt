03134080346d1a293427eb941ee65c8a2af87e93
yoontaebin <gusckd000@gmail.com>
2024-06-25 11:53:24 +0900

    Update :: 개인 일정도 반환되게 수정

--- src/main/kotlin/com/project/school/domain/school/application/service/FindSchoolScheduleService.kt
+++ src/main/kotlin/com/project/school/domain/school/application/service/FindSchoolScheduleService.kt
@@ -4,6 +4,7 @@ import com.project.school.common.annotation.ServiceWithReadOnlyTransaction
 import com.project.school.domain.account.application.exception.AccountNotFoundException
 import com.project.school.domain.account.application.port.output.AccountSecurityPort
 import com.project.school.domain.account.application.port.output.QueryAccountPort
+import com.project.school.domain.schedule.application.port.output.QuerySchedulePort
 import com.project.school.domain.school.adapter.output.neis.properties.NeisProperties
 import com.project.school.domain.school.application.port.input.FindSchoolScheduleUseCase
 import com.project.school.domain.school.application.port.input.dto.SchoolScheduleDto
@@ -13,9 +14,10 @@ import com.project.school.domain.school.application.port.output.FindSchoolSchedu
 class FindSchoolScheduleService(
     private val accountSecurityPort: AccountSecurityPort,
     private val queryAccountPort: QueryAccountPort,
+    private val querySchedulePort: QuerySchedulePort,
     private val neisProperties: NeisProperties,
     private val neisFindSchoolSchedulePort: FindSchoolSchedulePort
-): FindSchoolScheduleUseCase {
+) : FindSchoolScheduleUseCase {
 
     override fun execute(date: String): List<SchoolScheduleDto> {
         val accountIdx = accountSecurityPort.getCurrentAccountIdx()
@@ -30,12 +32,22 @@ class FindSchoolScheduleService(
             account.school.adminCode,
             date
         )
-        return schoolSchedule.map {
+        val schedule = querySchedulePort.findAllByDateAndAccount(date, account)
+
+        val scheduleResponse = schedule.map {
+            SchoolScheduleDto(
+                eventDate = it.date,
+                eventName = it.content
+            )
+        }
+        val schoolScheduleResponse = schoolSchedule.map {
             SchoolScheduleDto(
                 eventDate = it.eventDate,
                 eventName = it.eventName
             )
         }
+
+        return scheduleResponse + schoolScheduleResponse
     }
 
 }
