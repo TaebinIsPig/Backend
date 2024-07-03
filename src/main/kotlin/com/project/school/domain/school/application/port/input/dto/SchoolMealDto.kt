7f65f273faa9bd6d0c2427f5c7222d4834284c42
yoontaebin <gusckd000@gmail.com>
2024-07-03 23:47:35 +0900

    Delete :: dto 제거

--- src/main/kotlin/com/project/school/domain/school/application/port/input/dto/SchoolMealDto.kt
+++
@@ -1,8 +0,0 @@
-package com.project.school.domain.school.application.port.input.dto
-
-data class SchoolMealDto(
-    val mealType: String?,
-    val mealDate: String?,
-    val food: List<String>?,
-    val calorie: String?
-)
--- src/main/kotlin/com/project/school/domain/school/application/port/input/dto/SchoolScheduleDto.kt
+++
@@ -1,7 +0,0 @@
-package com.project.school.domain.school.application.port.input.dto
-
-data class SchoolScheduleDto(
-    val idx: Long?,
-    val eventDate: String?,
-    val eventName: String?
-)
