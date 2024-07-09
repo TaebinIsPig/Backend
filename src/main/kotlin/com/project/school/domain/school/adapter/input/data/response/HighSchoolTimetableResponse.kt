067a26aeb412ef6b0497872fe2e8da2913495f17
yoontaebin <gusckd000@gmail.com>
2024-07-09 12:14:41 +0900

    Update :: high timetable redis cache

--- src/main/kotlin/com/project/school/domain/school/adapter/input/data/response/HighSchoolTimetableResponse.kt
+++ src/main/kotlin/com/project/school/domain/school/adapter/input/data/response/HighSchoolTimetableResponse.kt
@@ -1,6 +1,9 @@
 package com.project.school.domain.school.adapter.input.data.response
 
+import com.project.school.domain.school.adapter.output.neis.response.NeisFindHighSchoolTimetableResponse
+
 data class HighSchoolTimetableResponse(
-    val period: String?,
-    val subject: String?
-)
+    val highSchoolTimetableList: List<NeisFindHighSchoolTimetableResponse>
+) {
+    constructor(): this(listOf())
+}
