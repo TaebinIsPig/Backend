59daf1550bd70599325776ae9c206442275c060b
yoontaebin <gusckd000@gmail.com>
2025-08-12 16:41:30 +0900

    Add :: CaffeineCacheAdapter

---
+++ src/main/kotlin/com/project/school/domain/school/adapter/output/cache/CaffeineCacheAdapter.kt
@@ -0,0 +1,19 @@
+package com.project.school.domain.school.adapter.output.cache
+
+import com.project.school.domain.school.application.port.output.cache.CachePort
+import org.springframework.cache.CacheManager
+import org.springframework.stereotype.Component
+
+@Component
+class CaffeineCacheAdapter(
+    private val cacheManager: CacheManager
+): CachePort {
+
+    override fun <T> get(cacheName: String, key: String, type: Class<T>): T? {
+        return cacheManager.getCache(cacheName)?.get(key, type)
+    }
+
+    override fun put(cacheName: String, key: String, value: Any) {
+        cacheManager.getCache(cacheName)?.put(key, value)
+    }
+}
