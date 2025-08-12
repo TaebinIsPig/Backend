f146eae310846df8a39b1e45c12cfbe2c2672330
yoontaebin <gusckd000@gmail.com>
2025-08-12 16:41:12 +0900

    Add :: Caffeine Cache Config

---
+++ src/main/kotlin/com/project/school/global/config/CacheConfig.kt
@@ -0,0 +1,30 @@
+package com.project.school.global.config
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import waffle.util.cache.CaffeineCache
+import java.util.concurrent.TimeUnit
+
+@EnableCaching
+@Configuration
+class CacheConfig {
+
+    @Bean
+    fun caffeineConfig(): Caffeine<Any, Any> =
+        Caffeine.newBuilder()
+            .expireAfterWrite(24, TimeUnit.HOURS)
+            .maximumSize(1000)
+            .recordStats()
+
+    @Bean
+    fun cacheManager(caffeine: Caffeine<Any, Any>): CacheManager =
+        CaffeineCacheManager(
+        ).apply {
+            setCaffeine(caffeine)
+        }
+}
